07c3d6aedf7dee63ba2732556f865c505781e526
mschreiber <martin@mschreiber.at>
2022-10-16 07:19:10 +0200

    Using Blazor Web Server App instead of Blazor WebAssembly

---
+++ CashRegister_DAL/Migrations/20221015131715_update.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CashRegister_DAL.Migrations
+{
+    public partial class update : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Belegnummer",
+                table: "Einkauf",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Kaufdatum",
+                table: "Einkauf",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Belegnummer",
+                table: "Einkauf");
+
+            migrationBuilder.DropColumn(
+                name: "Kaufdatum",
+                table: "Einkauf");
+        }
+    }
+}
---
+++ Test1/Data/VerlaufService.cs
@@ -0,0 +1,17 @@
+﻿using CashRegister.DAL;
+using CashRegister.Models;
+
+namespace Test1.Data
+{
+    public class VerlaufService
+    {
+        static CashRegisterContextDB context = new CashRegisterContextDB();
+
+        public Task<List<Einkauf>> GetVerlaufDaten()
+        {
+            return Task.FromResult(context.Einkauf.ToList());
+        }
+
+
+    }
+}
